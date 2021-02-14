8f15cde3fe537b594e264eda6dd9862a9e9a4cd0
delp <panglod@gmail.com>
2021-02-14 20:01:56 +0100

    Update extend.php

--- extend.php
+++ extend.php
@@ -5,5 +5,5 @@ namespace delp\BBCodeBars;
 use Illuminate\Contracts\Events\Dispatcher;
 return function (Dispatcher $events) {
     $events->subscribe(Listeners\AddBBCode::class);
-    $events->subscribe(Listeners\AddClientAssets::class);
+   
 };
