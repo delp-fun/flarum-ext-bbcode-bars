da5b194767e9277f1ad23116e8dd82d174f316ec
delp <panglod@gmail.com>
2021-02-14 20:01:31 +0100

    Update AddBBCode.php

--- src/Listeners/AddBBCode.php
+++ src/Listeners/AddBBCode.php
@@ -1,30 +1,23 @@
 <?php
 namespace oe800\BBCodeBars\Listeners;
-use Flarum\Event\ConfigureFormatter;
-use Illuminate\Contracts\Events\Dispatcher;
-class AddBBCode
-{
-    public function subscribe(Dispatcher $events)
-    {
-        $events->listen(ConfigureFormatter::class, [$this, 'addBBCode']);
-    }
-    public function addBBCode(ConfigureFormatter $event)
-    {
+use Flarum\Extend;
+use s9e\TextFormatter\Configurator;
 
+return [
+    (new Extend\Frontend('forum'))
+        ->css(__DIR__.'/less/forum.less')
 
-
-$event->configurator->BBCodes->addCustom(
-            '[PBAR]{TEXT},{TEXT2},{COLOR},{COLOR2},{COLOR3},{NUMBER},{NUMBER2},{NUMBER3},{NUMBER4}[/PBAR]',
-'<h1>{TEXT}</h1>
-    <div class="arrow" style="border: {NUMBER}px solid {COLOR};border-radius:{NUMBER3}px;margin-bottom:{NUMBER4}px">
-     <div class="arrow-status" style="width: {NUMBER2}%; background-color: {COLOR2};border-bottom-left-radius: {NUMBER3}px; border-top-left-radius: {NUMBER3}px;border-right: 0.5em solid {COLOR3}">
-       <span class="arrow-pointer">{TEXT2}</span>
-    </div>
-    </div>'
-
-);
-
-
-
-    }
+    (new Extend\Formatter())
+        ->configure(function (Configurator $configurator) {
+            $configurator->BBCodes->addCustom(
+                '[PBAR]{TEXT},{TEXT2},{COLOR},{COLOR2},{COLOR3},{NUMBER},{NUMBER2},{NUMBER3},{NUMBER4}[/PBAR]',
+                '<h1>{TEXT}</h1>
+                <div class="arrow" style="border: {NUMBER}px solid {COLOR};border-radius:{NUMBER3}px;margin-bottom:{NUMBER4}px">
+                 <div class="arrow-status" style="width: {NUMBER2}%; background-color: {COLOR2};border-bottom-left-radius: {NUMBER3}px; border-top-left-radius: {NUMBER3}px;border-right: 0.5em solid {COLOR3}">
+                   <span class="arrow-pointer">{TEXT2}</span>
+                </div>
+                </div>'
+            );
+        }),
+];
 }
